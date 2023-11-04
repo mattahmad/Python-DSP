8dd3b42b3012ec1e9ea8f10be6cb10b73f49f072
Matt Ahmad <matt.a@myplanet.com>
2023-11-04 16:14:30 -0400

    some reviewed fft scripts

--- sample/core.py
+++ sample/core.py
@@ -1,12 +1,23 @@
-# -*- coding: utf-8 -*-
-from . import helpers
-
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
 
 
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+import numpy as np
+import matplotlib.pyplot as plt
+
+Fs = 1 # Hz
+N = 100 # number of points to simulate, and our FFT size
+
+t = np.arange(N) # because our sample rate is 1 Hz
+s = np.sin(0.15*2*np.pi*t)
+S = np.fft.fftshift(np.fft.fft(s))
+S_mag = np.abs(S)
+S_phase = np.angle(S)
+f = np.arange(Fs/-2, Fs/2, Fs/N)
+plt.figure(0)
+plt.plot(f, S_mag,'.-')
+plt.figure(1)
+plt.plot(f, S_phase,'.-')
+plt.show()
+
+
+
+
